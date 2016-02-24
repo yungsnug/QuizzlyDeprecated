e2d0c405c0858750e960c1c598a0f4d3398b01b4
Mark Krant <mark.krant@gmail.com>
2016-02-23 21:21:22 -0800

    Quiz and answerper controllers skeleton

---
+++ api/controllers/AnswerPerProfessorController.js
@@ -0,0 +1,76 @@
+/**
+ * AnswerPerProfessorController
+ *
+ * @description :: Server-side logic for managing Answerperprofessors
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+
+
+  /**
+   * `AnswerPerProfessorController.create()`
+   */
+  create: function (req, res) {
+    return res.json({
+      todo: 'create() is not implemented yet!'
+    });
+  },
+
+	/**
+   * `AnswerPerProfessorController.show()`
+   */
+  show: function (req, res) {
+    return res.json({
+      todo: 'show() is not implemented yet!'
+    });
+  },
+
+	/**
+	 * `AnswerPerProfessorController.isCorrect()`
+	 */
+	isCorrect: function (req, res) {
+		return res.json({
+			todo: 'show() is not implemented yet!'
+		});
+	},
+
+	/**
+	 * `AnswerPerProfessorController.getCorrectAnswer()`
+	 */
+	getCorrectAnswer: function (req, res) {
+		return res.json({
+			todo: 'show() is not implemented yet!'
+		});
+	},
+
+  /**
+   * `AnswerPerProfessorController.getProfessor()`
+   */
+  getProfessor: function (req, res) {
+    return res.json({
+      todo: 'show() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `AnswerPerProfessorController.edit()`
+   */
+  edit: function (req, res) {
+    return res.json({
+      todo: 'edit() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `AnswerPerProfessorController.delete()`
+   */
+  delete: function (req, res) {
+    return res.json({
+      todo: 'delete() is not implemented yet!'
+    });
+  }
+};
---
+++ api/controllers/QuizController.js
@@ -0,0 +1,85 @@
+/**
+ * QuizController
+ *
+ * @description :: Server-side logic for managing Quizzes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+
+
+  /**
+   * `QuizController.create()`
+   */
+  create: function (req, res) {
+    return res.json({
+      todo: 'create() is not implemented yet!'
+    });
+  },
+
+	/**
+   * `QuizController.getTitle()`
+   */
+  getTitle: function (req, res) {
+    return res.json({
+      todo: 'show() is not implemented yet!'
+    });
+  },
+
+	/**
+	 * `QuizController.getQuestions()`
+	 */
+	getQuestions: function (req, res) {
+		return res.json({
+			todo: 'show() is not implemented yet!'
+		});
+	},
+
+	/**
+	 * `QuizController.getQuestion()`
+	 */
+	getQuestion: function (req, res) {
+		return res.json({
+			todo: 'show() is not implemented yet!'
+		});
+	},
+
+	/**
+	 * `QuizController.getProfessor()`
+	 */
+	getProfessor: function (req, res) {
+		return res.json({
+			todo: 'show() is not implemented yet!'
+		});
+	},
+
+  /**
+   * `QuizController.show()`
+   */
+  show: function (req, res) {
+    return res.json({
+      todo: 'show() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `QuizController.edit()`
+   */
+  edit: function (req, res) {
+    return res.json({
+      todo: 'edit() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `QuizController.delete()`
+   */
+  delete: function (req, res) {
+    return res.json({
+      todo: 'delete() is not implemented yet!'
+    });
+  }
+};
